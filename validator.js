e9c083ada8393751b0ed8c7c714829d42e12958b
l3bao84 <l3ducb4008@gmail.com>
2022-05-26 18:43:35 +0700

    commit on 'master'

---
+++ validator.js
@@ -0,0 +1,46 @@
+function Validator(options) {
+
+    var formElement = document.querySelector(options.form)
+    if(formElement) {
+
+        options.rules.forEach(function(rule) {
+
+            var inputElement = formElement.querySelector(rule.selector)
+            var errorElement = inputElement.parentElement.querySelector('.form-message')
+            if(inputElement) {
+                
+                inputElement.onblur = function() {
+
+                    var errorMessage = rule.test(inputElement.value)
+                    if(errorMessage) {
+
+                        errorElement.innerText = errorMessage
+                        inputElement.parentElement.classList.add('invalid')
+                    }else {
+
+                        errorElement.innerText = ''
+                        inputElement.parentElement.classList.remove('invalid')
+                    }
+                }
+            }
+        })
+    }
+}
+
+Validator.isEmail = function(selector) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value.trim() ? undefined : 'Vui lòng nhập trường này'
+        }
+    }
+}
+
+Validator.isPassword = function(selector) {
+    return {
+        selector: selector,
+        test: function() {
+            
+        }
+    }
+}
