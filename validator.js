d21391f5bac8d8c3936072d80b5b46377328d4bd
l3bao84 <l3ducb4008@gmail.com>
2022-05-26 20:59:46 +0700

    commit on 'master'

--- validator.js
+++ validator.js
@@ -1,26 +1,38 @@
 function Validator(options) {
 
+    //Hàm validate
+    function validate(inputElement, rule) {
+        var errorMessage = rule.test(inputElement.value)
+        var errorElement = inputElement.parentElement.querySelector(options.errorSelector)
+            if(errorMessage) {
+
+                errorElement.innerText = errorMessage
+                inputElement.parentElement.classList.add('invalid')
+            }else {
+
+                errorElement.innerText = ''
+                inputElement.parentElement.classList.remove('invalid')
+            }
+    }
+
+    //Lấy element từ form vầ xác nhận
     var formElement = document.querySelector(options.form)
     if(formElement) {
 
         options.rules.forEach(function(rule) {
 
             var inputElement = formElement.querySelector(rule.selector)
-            var errorElement = inputElement.parentElement.querySelector('.form-message')
             if(inputElement) {
-                
+                //Blur ngoài input
                 inputElement.onblur = function() {
-
-                    var errorMessage = rule.test(inputElement.value)
-                    if(errorMessage) {
-
-                        errorElement.innerText = errorMessage
-                        inputElement.parentElement.classList.add('invalid')
-                    }else {
-
-                        errorElement.innerText = ''
-                        inputElement.parentElement.classList.remove('invalid')
-                    }
+                    validate(inputElement,rule)
+                }
+                
+                //Xử lý khi nhập
+                inputElement.oninput = function() {
+                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector)
+                    errorElement.innerText = ''
+                    inputElement.parentElement.classList.remove('invalid')
                 }
             }
         })
@@ -31,7 +43,8 @@ Validator.isEmail = function(selector) {
     return {
         selector: selector,
         test: function(value) {
-            return value.trim() ? undefined : 'Vui lòng nhập trường này'
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+            return regex.test(value) ? undefined : 'Trường này phải là email!'
         }
     }
 }
@@ -39,8 +52,18 @@ Validator.isEmail = function(selector) {
 Validator.isPassword = function(selector) {
     return {
         selector: selector,
-        test: function() {
-            
+        test: function(value) {
+            var regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/
+            return regex.test(value) ? undefined : 'Mật khẩu phải ít nhất 8 kí tự(chỉ gồm chữ hoa, thường, số và không có kí tự đặc biệt'
+        }
+    }
+}
+
+Validator.isConfirmed = function(selector, getConfirmValue) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value === getConfirmValue() ? undefined : 'Mật khẩu nhập lại không chính xác'
         }
     }
 }
